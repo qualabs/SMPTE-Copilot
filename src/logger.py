538e6439f07b984acfab4391159ed55110c1eb76
Nicolas Caballero <nicolascaballero.v@gmail.com>
2025-12-30 14:56:10 -0300

    improve loggerrr

--- src/logger.py
+++ src/logger.py
@@ -11,7 +11,8 @@ class Logger:
     def setup(config) -> None:
         logging.basicConfig(
             level=config.logging.get_level(),
-            format="%(name)s - %(message)s",
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            datefmt="%H:%M:%S",
             stream=sys.stdout,
             force=True,  # Override any existing configuration
         )
