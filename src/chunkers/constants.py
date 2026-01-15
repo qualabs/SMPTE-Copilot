e8bfe59ac8677fec79d0fccb17bed333965a250e
Nicolas Caballero <nicolascaballero.v@gmail.com>
2026-01-15 20:33:16 -0300

    remove some comments on constants.py for tokenizers

--- src/chunkers/constants.py
+++ src/chunkers/constants.py
@@ -12,7 +12,6 @@ DEFAULT_CHUNK_OVERLAP = 200
 CHUNK_INDEX_METADATA_KEY = "chunk_index"
 TOTAL_CHUNKS_METADATA_KEY = "total_chunks"
 
-# Token-based splitting heuristics
 CHARS_PER_TOKEN_RATIO = 1.5  # Conservative estimate for char-to-token ratio
-SPLIT_BUFFER_SIZE = 5  # Number of words to buffer before checking limits
+SPLIT_BUFFER_SIZE = 20
 
